f2939b5e8ea2b79f071ef7771c7f781156944f60
Gregory Shein <13556916+kurumbus@users.noreply.github.com>
2024-12-22 20:49:49 +0300

    multi gpu

--- train_lapa.py
+++ train_lapa.py
@@ -1,19 +1,16 @@
 import os
-import random
 import json
-
 import torch
-from trainer import Trainer, TrainerArgs
-from TTS.utils.audio import AudioProcessor
 import torch.distributed as dist
+import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler
-import torch.multiprocessing as mp
 
+from TTS.utils.audio import AudioProcessor
 from notdatasets.preprocess import load_wav_feat_spk_data
 from configs.gpt_hifigan_config import GPTHifiganConfig
 from models.gpt_gan import GPTGAN
-
+from trainer import Trainer, TrainerArgs
 
 class GPTHifiganTrainer:
     def __init__(self, config, rank, world_size):
@@ -26,6 +23,7 @@ class GPTHifiganTrainer:
         print(f"Files in mel path: {os.listdir(config.mel_path)}")
         print(f"Files in speaker path: {os.listdir(config.spk_path)}")
 
+        # Load data
         self.eval_samples, self.train_samples = load_wav_feat_spk_data(
             config.data_path, config.mel_path, config.spk_path, eval_split_size=config.eval_split_size
         )
@@ -42,8 +40,7 @@ class GPTHifiganTrainer:
         # Initialize model
         self.model = GPTGAN(config, self.ap)
 
-        # DDP Initialization
-        dist.init_process_group(backend='nccl', rank=self.rank, world_size=self.world_size)
+        # Move model to GPU and wrap it in DDP
         self.model = self.model.to(self.rank)  # Move model to the current GPU
         self.model = DDP(self.model, device_ids=[self.rank])
 
@@ -51,8 +48,7 @@ class GPTHifiganTrainer:
         if config.pretrain_path is not None:
             state_dict = torch.load(config.pretrain_path, map_location='cuda')
             hifigan_state_dict = {
-                k.replace("xtts.hifigan_decoder.waveform_decoder.", "").replace("hifigan_decoder.waveform_decoder.",
-                                                                                ""): v
+                k.replace("xtts.hifigan_decoder.waveform_decoder.", "").replace("hifigan_decoder.waveform_decoder.", ""): v
                 for k, v in state_dict["model"].items()
                 if "hifigan_decoder" in k and "speaker_encoder" not in k
             }
@@ -60,8 +56,7 @@ class GPTHifiganTrainer:
 
             if config.train_spk_encoder:
                 speaker_encoder_state_dict = {
-                    k.replace("xtts.hifigan_decoder.speaker_encoder.", "").replace("hifigan_decoder.waveform_decoder.",
-                                                                                   ""): v
+                    k.replace("xtts.hifigan_decoder.speaker_encoder.", "").replace("hifigan_decoder.waveform_decoder.", ""): v
                     for k, v in state_dict["model"].items()
                     if "hifigan_decoder" in k and "speaker_encoder" in k
                 }
@@ -73,13 +68,13 @@ class GPTHifiganTrainer:
 
         # Initialize the trainer
         trainer = Trainer(
-            TrainerArgs(), config, config.output_path, model=self.model, train_samples=self.train_samples,
-            eval_samples=self.eval_samples, sampler=train_sampler
+            TrainerArgs(), self.config, self.config.output_path, model=self.model,
+            train_samples=self.train_samples, eval_samples=self.eval_samples, sampler=train_sampler
         )
         trainer.fit()
 
 def main_worker(rank, world_size, config):
-    # Set the environment variables for distributed training
+    # Set environment variables for distributed training
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '29500'
     os.environ['WORLD_SIZE'] = str(world_size)
@@ -88,16 +83,19 @@ def main_worker(rank, world_size, config):
     # Initialize the distributed process group
     dist.init_process_group(backend='nccl', rank=rank, world_size=world_size)
 
-    # Now initialize the trainer
+    # Initialize and train the model
     trainer = GPTHifiganTrainer(config, rank, world_size)
     trainer.train()
 
-if __name__ == "__main__":
+    # Cleanup after training
+    dist.destroy_process_group()
+
+def main():
     world_size = torch.cuda.device_count()  # Number of GPUs
     config = GPTHifiganConfig(**json.load(open("config_v00.json", "r")))  # Load config file
 
+    # Launch the distributed training processes
     mp.spawn(main_worker, args=(world_size, config), nprocs=world_size, join=True)
-  
 
-    hifigan_trainer = GPTHifiganTrainer(config=config)
-    hifigan_trainer.train()
+if __name__ == "__main__":
+    main()
